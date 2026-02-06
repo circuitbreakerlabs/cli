217b679e672340806461c5189b2cc7959652ea18
Wyatt Avilla <wyattmurphy1@gmail.com>
2026-02-06 15:46:47 -0800

    rm nix-only change

--- flake.nix
+++ flake.nix
@@ -7,8 +7,6 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  # change to trigger ci
-
   outputs =
     { self, nixpkgs, ... }@inputs:
     inputs.flake-utils.lib.eachDefaultSystem (
