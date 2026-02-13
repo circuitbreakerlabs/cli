58fc3e6343422c05e04c90296b8bbfd6b45a210c
Wyatt Avilla <wyatt@linux.com>
2026-02-13 14:09:49 -0800

    add `ConversationError` type

--- src/protocol_types/common/optional.rs
+++ src/protocol_types/common/optional.rs
@@ -16,3 +16,22 @@ pub struct UnsafeMessageEnvelope {
     /// Unsafe message payload
     pub data: UnsafeMessage,
 }
+
+/// Payload for conversation_error messages (Server -> Client).
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ConversationError {
+    /// Identifier for the completed conversation
+    pub conversation_id: i32,
+    /// Details about the error that occurred during processing
+    pub error_message: String,
+}
+
+/// Server notifies client that an error occurred while processing a conversation.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ConversationErrorEnvelope {
+    /// Message type
+    #[serde(rename = "type")]
+    pub message_type: String,
+    /// Conversation error payload
+    pub data: ConversationError,
+}
