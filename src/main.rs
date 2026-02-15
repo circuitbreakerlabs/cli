ccafc86a59b0c331bfd32a5cde3fc4589b794865
Wyatt Avilla <wyatt@linux.com>
2026-02-15 14:49:59 -0800

    wire up main

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,10 @@ mod protocol_types;
 mod response_provider;
 mod websockets;
 
+use std::sync::Arc;
+
 use clap::Parser;
-
-async fn run_single_turn_evaluation(request: protocol_types::SingleTurnRequest) {
-    todo!("Implement single-turn evaluation logic");
-}
-
-async fn run_multi_turn_evaluation(request: protocol_types::MultiTurnRequest) {
-    todo!("Implement multi-turn evaluation logic");
-}
+use response_provider::{AnthropicProvider, OllamaProvider, OpenAIProvider, ResponseProvider};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -23,12 +18,50 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .with_max_level(Into::<tracing::Level>::into(cli_args.log_level))
         .init();
 
-    match cli_args.command {
-        cli::EvalCommand::SingleTurn(request) => {
-            run_single_turn_evaluation(request).await;
+    let provider_command = match &cli_args.evaluation {
+        cli::EvaluationCommand::SingleTurn { provider, .. }
+        | cli::EvaluationCommand::MultiTurn { provider, .. } => provider,
+    };
+
+    let provider = match provider_command {
+        cli::ProviderCommand::Ollama(config) => {
+            Arc::new(OllamaProvider::new(config.clone())?) as Arc<dyn ResponseProvider>
         }
-        cli::EvalCommand::MultiTurn(request) => {
-            run_multi_turn_evaluation(request).await;
+        cli::ProviderCommand::OpenAI(config) => {
+            Arc::new(OpenAIProvider::new(config.clone())?) as Arc<dyn ResponseProvider>
+        }
+        cli::ProviderCommand::Anthropic(config) => {
+            Arc::new(AnthropicProvider::new(config.clone())?) as Arc<dyn ResponseProvider>
+        }
+    };
+
+    let websocket = {
+        let endpoint = match &cli_args.evaluation {
+            cli::EvaluationCommand::SingleTurn { .. } => consts::endpoints::SINGLE_TURN_ENDPOINT,
+            cli::EvaluationCommand::MultiTurn { .. } => consts::endpoints::MULTI_TURN_ENDPOINT,
+        };
+
+        websockets::connect(
+            format!("{}/{}", cli_args.cbl_api_base_url, endpoint).as_str(),
+            &cli_args.cbl_api_key,
+        )
+        .await?
+    };
+
+    match cli_args.evaluation {
+        cli::EvaluationCommand::SingleTurn { request, .. } => {
+            let result = evaluations::singleturn::run_evaluation(websocket, provider, request)
+                .await
+                .map_err(|e| e.to_string())?;
+
+            println!("Single-turn evaluation result: {result:?}");
+        }
+        cli::EvaluationCommand::MultiTurn { request, .. } => {
+            let result = evaluations::multiturn::run_evaluation(websocket, provider, request)
+                .await
+                .map_err(|e| e.to_string())?;
+
+            println!("Multi-turn evaluation result: {result:?}");
         }
     }
 
