8b569a7ee6053d2621284ca2954f74c6d98f26bc
Wyatt Avilla <wyatt@linux.com>
2026-02-14 17:37:16 -0800

    response provider trait

--- src/response_provider/mod.rs
+++ src/response_provider/mod.rs
@@ -1,4 +1,16 @@
 mod completion_generator;
+pub mod ollama;
 mod types;
 
+use crate::protocol_types;
+use async_trait::async_trait;
+
 pub use completion_generator::CompletionGenerator;
+
+#[async_trait]
+pub trait ResponseProvider: Send + Sync {
+    async fn generate_response(
+        &self,
+        conversation_history: &[protocol_types::Message],
+    ) -> Result<protocol_types::Message, Box<dyn std::error::Error>>;
+}
