b9140ff58aded448fcc2893709a49585f79b2747
Wyatt Avilla <wyatt@linux.com>
2026-02-27 16:36:53 -0800

    clone and debug for progress indicator types

--- src/tui/mod.rs
+++ src/tui/mod.rs
@@ -1,13 +1,15 @@
-mod multiturn;
+pub mod multiturn;
 mod singleturn;
-pub use multiturn::MultiTurnProgressIndicatorMessage;
+pub use multiturn::{MultiTurnProgressIndicatorMessage, render_task};
 
+#[derive(Clone, Debug)]
 pub enum WaitingFor {
     Provider,
     #[allow(clippy::upper_case_acronyms)]
     API,
 }
 
+#[derive(Clone, Debug)]
 pub enum ConversationStatus {
     Waiting(WaitingFor),
     Passed,
